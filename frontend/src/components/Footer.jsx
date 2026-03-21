9a8573fa8973eef69cd2e6c503ad766c7303600f
shivansh <shivansh@example.com>
2026-03-21 23:20:58 +0530

    modified

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,10 +5,12 @@ function Footer() {
   return (
     <div>
           <div className='flex flex-col  sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+              
               <div>
                   <img src={assets.logo} className='mb-5 w-32' alt="" />
                   <p className='w-full md:w-2/3 text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, hic reiciendis. In similique porro blanditiis laboriosam consectetur nesciunt nisi eos velit nostrum?</p>
               </div>
+
               <div>
                   <p className='text-xl font-medium mb-5'>Company</p>
                   <ul className='flex flex-col gap-1 text-gray-600'>
